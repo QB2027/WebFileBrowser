23a7cba31623fe46e2176db43843e486cef0a8ec
jasonxue <103628493+jasonxue1@users.noreply.github.com>
2025-01-07 14:16:51 +0800

    Update utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -2,6 +2,7 @@
 
 /**
  * 获取当前 URL 中的 ?path= 值
+ * @returns {string} - 当前目录路径
  */
 export function getCurrentDirectory() {
   const params = new URLSearchParams(window.location.search);
@@ -10,6 +11,8 @@ export function getCurrentDirectory() {
 
 /**
  * 从当前 path 中移除最后一级，得到上一级路径
+ * @param {string} path - 当前目录路径
+ * @returns {string} - 上一级目录路径
  */
 export function getParentPath(path) {
   const segments = path.split('/').filter(Boolean);
