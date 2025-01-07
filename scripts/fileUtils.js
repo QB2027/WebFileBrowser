b51e3beb997206b94bfdfcc838e7e5b3fbe88bdb
jasonxue <103628493+jasonxue1@users.noreply.github.com>
2025-01-07 14:16:34 +0800

    Update fileUtils.js

--- scripts/fileUtils.js
+++ scripts/fileUtils.js
@@ -2,6 +2,9 @@
 
 /**
  * 过滤出指定 path 下的内容
+ * @param {Array} files - 所有文件和文件夹的数组
+ * @param {string} path - 当前目录路径
+ * @returns {Array} - 当前目录下的文件和文件夹
  */
 export function filterFilesByPath(files, path) {
   return files.filter(item => {
