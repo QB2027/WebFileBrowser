ae5d5c5ab47106976caa6f0a34bfb27d0f76b1bf
jasonxue <103628493+jasonxue1@users.noreply.github.com>
2025-01-07 14:07:31 +0800

    Update generate_files_json.py

--- generate_files_json.py
+++ generate_files_json.py
@@ -7,8 +7,8 @@ import os
 # 获取环境变量中的 OSS 凭证
 access_key_id = os.getenv('OSS_ACCESS_KEY_ID')
 access_key_secret = os.getenv('OSS_ACCESS_KEY_SECRET')
-bucket_name = 'class-files'  # 替换为你的 Bucket 名称
-endpoint = 'oss-cn-hangzhou.aliyuncs.com'  # 根据你的 Bucket 所在区域调整
+bucket_name = 'qb2027-files'  # 替换为你的 Bucket 名称
+endpoint = 'oss-cn-shanghai.aliyuncs.com'  # 根据你的 Bucket 所在区域调整
 
 # 初始化 OSS Bucket
 auth = oss2.Auth(access_key_id, access_key_secret)
