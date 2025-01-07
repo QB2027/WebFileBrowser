ca511a1652d1ec6c8d635c4dc3d918ae0c484ec1
jasonxue <103628493+jasonxue1@users.noreply.github.com>
2025-01-07 13:44:30 +0800

    Update generate_files_json.py

--- generate_files_json.py
+++ generate_files_json.py
@@ -7,14 +7,14 @@ import os
 access_key_id = os.getenv('OSS_ACCESS_KEY_ID')
 access_key_secret = os.getenv('OSS_ACCESS_KEY_SECRET')
 bucket_name = 'class-files'  # 替换为你的 Bucket 名称
-endpoint = 'oss-cn-hangzhou.aliyuncs.com'  # 根据你的Bucket所在区域调整
+endpoint = 'oss-cn-shanghai.aliyuncs.com'  # 根据你的Bucket所在区域调整
 
 # 初始化 OSS Bucket
 auth = oss2.Auth(access_key_id, access_key_secret)
 bucket = oss2.Bucket(auth, endpoint, bucket_name)
 
 # 要扫描的目录前缀
-prefix = '/'
+prefix = ''
 
 # 获取所有文件列表
 file_list = []
